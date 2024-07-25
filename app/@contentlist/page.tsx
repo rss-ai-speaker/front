bcb93960f98c8b3ae24a816cac287d83d1806e7d
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2024-07-25 10:24:17 +0900

    feat: ellipsis 함수 추가

--- app/@contentlist/page.tsx
+++ app/@contentlist/page.tsx
@@ -9,6 +9,9 @@ interface Content {
   created_at: string;
 }
 
+const ellipsis = (text: string, length: number) =>
+  text.length > length ? text.slice(0, length) + "..." : text;
+
 const ContentListPage = async () => {
   const response = await fetch(`${BASE_URL}/api/list`, {
     cache: "no-cache",
@@ -18,16 +21,17 @@ const ContentListPage = async () => {
   return (
     <>
       <h1 className=" text-lg">Content List Page</h1>
-      <ul>
+      <ul className="flex flex-col gap-4">
         {contentList?.map(({ id, summary }) => {
           const title = summary.split("\n")[0];
+          const ellipsisTitle = ellipsis(title, 30);
           return (
             <Link
               href={`/feed-summary?id=${id}`}
               key={id}
               className="text-ellipsis"
             >
-              <Markdown>{title}</Markdown>
+              <Markdown>{ellipsisTitle}</Markdown>
             </Link>
           );
         })}
