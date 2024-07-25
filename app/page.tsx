73523d74a3443e31965230a42e20eb28e440c8f5
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2024-07-25 11:47:37 +0900

    refactor: feedSummary 페이지를 루트로 이동

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
-const Home = () => {
-  return <div>Home</div>;
-};
+import { BASE_URL } from "@/app/apis";
+import Markdown from "markdown-to-jsx";
 
-export default Home;
+interface PageProps {
+  searchParams: {
+    id: string;
+  };
+}
+
+export default async function Page({ searchParams }: PageProps) {
+  const feedSummary = await fetch(
+    `${BASE_URL}/api/summarize?id=${searchParams.id}`,
+    { next: { tags: ["feedSummary", searchParams.id] } }
+  );
+  const markdownText = await feedSummary.text();
+  return <Markdown>{markdownText}</Markdown>;
+}
